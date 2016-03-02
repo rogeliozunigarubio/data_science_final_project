58bdc7aa1864f466e434465fd45128ea585acef2
rogeliozunigarubio <roger.zuniga@gmail.com>
2016-03-02 09:11:38 -0500

    fixed unicode

--- capture-tweets.py
+++ capture-tweets.py
@@ -3,16 +3,15 @@ import sys
 
 from TwitterSearch import *
 
-out = open('test-captured-tweets.txt', 'ab')
+out = open('test-kingston-tweets.txt', 'ab')
 
 try:
-    tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['beach','trash']) # let's define all words we would like to have a look for
-    #tso.set_geocode(18.471215,-77.9398878,100,True) # we want to see German tweets only
-    tso.set_language('en') # we want to see German tweets only
-    tso.set_include_entities(False) # and don't give us all those entity information
+    tso = TwitterSearchOrder()
+    tso.set_keywords(['beach','trash'])
+    tso.set_geocode(18.0180869,-76.8708689,100,True) #Kingston Jamaica Test
+    tso.set_language('en')
+    tso.set_include_entities(False)
 
-    # it's about time to create a TwitterSearch object with our secret tokens
     ts = TwitterSearch(
         consumer_key = 'FnrBL5aEee3HVWNz6cyZFbFWn',
         consumer_secret = "ApRZXcUrn0RSRTRYuNEKCnT1u7M3q4AF1HLOE4EIlZwra5Vxi4",
@@ -22,10 +21,10 @@ try:
 
 
     for tweet in ts.search_tweets_iterable(tso):
-        #out.write( (str(tweet['text']) + "\n" ).encode('utf-8') )
+        out.write( tweet['text'].encode('utf-8')  + "\n" )
         print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
 
     print(ts.get_statistics())
 
-except TwitterSearchException as e: # take care of all those ugly errors if there are some
+except TwitterSearchException as e: 
     print(e)
