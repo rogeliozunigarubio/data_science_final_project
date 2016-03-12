a9bdd0fbbc5215e6bd950ba08e0c56b18b7adaf7
rogeliozunigarubio <roger.zuniga@gmail.com>
2016-03-11 19:39:36 -0500

    more data, elbow finally works

---
+++ tripadvisor_downloader.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147365-d2198364
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147367-d2385044
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147366-d4729113
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147249-d150447
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147249-d539183
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147249-d148915-
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g488162-d2513634
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147313-d148623
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147313-d148623
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147313-d148623
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147312-d1997485
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147312-d1997485
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g666621-d4053401
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147264-d149093
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g1183194-d555825
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147342-d150064
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147342-d149013
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147342-d149221
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g147293-d215733
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g259440-d2006612
+wget --convert-links --domains www.tripadvisor.com --recursive --no-parent https://www.tripadvisor.com/Attraction_Review-g317146-d149676
